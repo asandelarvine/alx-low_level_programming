8726336be2b4ef46b41037012d31be327d581ef9
asandelarvine <55802027+asandelarvine@users.noreply.github.com>
2022-04-17 13:54:11 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,24 +1,23 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * more_numbers - prints the range 0-14 ten times.
- * Return: nothing.
+ * more_numbers - nmbr fct
+ *
+ * Return: no return
  */
+
 void more_numbers(void)
 {
-  int i, j;
 
-  i = j = 0;
-  while (i < 10)
-    {
-      while (j <= 14)
+int i, j;
+	for (i = 1; i <= 10; i++)
 	{
-	  if (j >= 10)
-	    _putchar(j / 10 + '0');
-	  _putchar(j % 10 + '0');
-	  ++j;
-	}
-      _putchar('\n');
-      j = 0;
-      i++;
-    }
+		for (j = 0; j < 15; j++)
+		{
+			if (j >= 10)
+			_putchar('1');
+			_putchar(j % 10 + '0');
+		}
+			_putchar('\n');
+		}
 }
