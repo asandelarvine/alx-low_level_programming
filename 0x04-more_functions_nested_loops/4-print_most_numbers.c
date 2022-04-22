a228c3f874821251337d89063f0c8d668d8b5613
asandelarvine <55802027+asandelarvine@users.noreply.github.com>
2022-04-22 09:06:34 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,21 +1,12 @@
-#include "holberton.h"
-/**
- * print_most_numbers - print all numbers except 2 and 4
- * Return: 0 on success.
- */
-<<<<<<< HEAD
-void print_most_numbers(void)
-{
-  int i = 0;
+#include "main.h"
 
-  while (i <= 9)
-    {
-      if (i != 2 && i != 4)
-	_putchar(i + '0');
-      ++i;
-    }
-  _putchar('\n');
-=======
+/**
+ * print_most_numbers - entry point
+ *
+ * Description: Prints the numbers with _putchar
+ *
+ * Return: void
+ */
 
 void print_most_numbers(void)
 {
@@ -29,5 +20,4 @@ void print_most_numbers(void)
 			_putchar(number);
 	}
 	_putchar('\n');
->>>>>>> 5cef6008c6e5d0d931e686e29a257688d243c586
 }
