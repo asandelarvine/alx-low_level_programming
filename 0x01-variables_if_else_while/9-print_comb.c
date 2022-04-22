2999c2c25dfadff8410368b1c14b411b6b8bfe69
asandelarvine <55802027+asandelarvine@users.noreply.github.com>
2022-04-22 09:37:19 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers between 0 to 9 with commas and spaces.
- *
+ * main - prints all possible combinations of single-digit numbers
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-  int i;
+	int n;
 
-  for (i = 48; i < 58; i++)
-    {
-      putchar(i);
-      if (i != 57)
+	for (n = 48; n < 58; n++)
 	{
-	  putchar(',');
-	  putchar(' ');
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-    }
-  putchar('\n');
-  return (0);
+	putchar('\n');
+	return (0);
 }
