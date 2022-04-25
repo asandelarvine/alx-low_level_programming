622522dda562d0087598342ed1559794967e7e41
asandelarvine <55802027+asandelarvine@users.noreply.github.com>
2022-04-25 18:36:45 +0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,6 +8,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-  strcpy(dest, src);
-  return (dest);
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
+	return (dest);
 }
