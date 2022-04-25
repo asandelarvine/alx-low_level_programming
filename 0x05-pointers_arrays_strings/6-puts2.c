6e9cbc2ad93392231d08d93829f4cbeb8f8c63f6
asandelarvine <55802027+asandelarvine@users.noreply.github.com>
2022-04-25 18:32:23 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,13 +7,15 @@
 
 void puts2(char *str)
 {
-  int len = strlen(str);
-  int i;
+	int i = 0;
 
-  for (i = 0; i < len; i++)
-    {
-      if (i % 2 == 0)
-	_putchar(str[i]);
-    }
-  _putchar('\n');
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
 }
