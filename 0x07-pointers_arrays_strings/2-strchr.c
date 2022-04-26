6fccd70651ddfd95959943791e03da6bce5ef5de
asandelarvine <55802027+asandelarvine@users.noreply.github.com>
2022-04-26 18:06:38 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,13 @@
 
 char *_strchr(char *s, char c)
 {
-    int index;
+	int index;
 
-    for (index = 0; s[index] >= '\0'; index++)
-    {
-      if (s[index] == c)
-	return (s + index);
-    }
+	for (index = 0; s[index] >= '\0'; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
 
-    return ('\0');
+	return ('\0');
 }
