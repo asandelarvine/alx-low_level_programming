d2e07533c167a5302288a0500c36ff70f6dc38ef
larvine <larvine@kali.larvine>
2023-09-29 06:33:32 +0300

    addind missing protptype for the function (set_bit)

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,7 +3,7 @@
 
 /*
  * Author: Asande Larvine.
- * Desc: Header file containing prototypes for all functions
+ * Description: Header file containing prototypes for all functions
  *       written in the 0x13-bit_manipulation directory.
  */
 
